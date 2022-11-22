0bcd8900c13fb83eeed63489608b3d495baad7fc
NA-Siddiky <siddiky.academic@gmail.com>
2022-11-22 11:38:06 +0600

    starting template

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="grid grid-cols-12 h-screen">
+      <div className="bg-slate-900 col-span-2">Sidebar</div>
+      <div className="bg-slate-600 col-span-10">Main Content</div>
+    </div>
+  );
+}
