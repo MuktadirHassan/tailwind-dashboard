924ac8b331f1e328c7effd30adabca5a6c44ccd2
Muktadir Hassan <46109431+MuktadirHassan@users.noreply.github.com>
2023-04-26 20:06:13 +0600

    Revert "Siddiky"

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {
-      colors: {
-        "dark-purple": "#081A51",
-        "light-white": "rgba(255,255,255,0.17)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
